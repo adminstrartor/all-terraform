14acd58fc126fbcf8dbe2b21ca31cec25ffca3bc
adminstrator <root@ip-172-31-23-196.ec2.internal>
2024-05-19 10:22:13 +0000

    gitcommand

---
+++ gitdoc.sh
@@ -0,0 +1,28 @@
+pt update -y
+apt install git -y
+git --version
+git config --global user.name "name"
+git config --global user.emai "email"
+
+which git
+mkdir terraformcode
+cd terraform
+git init
+getting config gile.git hidden file
+create file under terraformcode
+vi terraform-file
+you can put your code
+git status
+git add .
+git commit -m "my first commit terraform"
+git log
+git show commitid
+git remote add origin gitcodeurl
+git remote set-url origin https://token@github.com/adminstrartor/all-terraform
+git push -u origin master
+
+
+git remote set-url origin https://token@github.com/adminstrartor/terraform-file
+
+git remote set-url origin https://<token>@github.com/<username>/<repo>
+
